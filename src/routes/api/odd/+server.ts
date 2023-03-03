83c7dab8216690ab9799c2ff6032684ebf3a1558
colemannoah <colemanmnoah@gmail.com>
2023-03-03 22:09:48 +0000

    server: error handling, i forgot

--- src/routes/api/odd/+server.ts
+++ src/routes/api/odd/+server.ts
@@ -3,8 +3,16 @@ import { Configuration, OpenAIApi } from 'openai';
 import { OPENAI_API_KEY } from '$env/static/private';
 
 export const POST = (async ({ request }) => {
+  if (!OPENAI_API_KEY) {
+    throw new Error('No OpenAI API key found');
+  }
+
   const { prompt } = await request.json();
   let result = '';
+
+  if (!prompt) {
+    throw new Error('No prompt provided');
+  }
 
   const config = new Configuration({
 		apiKey: OPENAI_API_KEY,
